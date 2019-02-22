b070b921671ef550705db81397d5e7774719f5bd
Carlo <carlo.dri@elettra.eu>
2019-02-22 14:06:25 +0100

    add some env vars
    take from here: https://github.com/conda-forge/portaudio-feedstock/blob/master/recipe/bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,9 @@
 COPY %RECIPE_DIR%\build.sh build.sh
+set PREFIX=%PREFIX:\=/%
+set SRC_DIR=%SRC_DIR:\=/%
+set MSYSTEM=MINGW%ARCH%
+set MSYS2_PATH_TYPE=inherit
+set CHERE_INVOKING=1
 
 bash -lc "ln -s ${LOCALAPPDATA}/Temp /tmp"
 bash -lc "./build.sh"
