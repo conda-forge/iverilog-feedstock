efc43443c654ad275b793cf73dddfb1a3e035048
Carlo <carlo.dri@gmail.com>
2026-02-23 08:35:18 +0100

    try if less is more.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -euxo pipefail
 
-if [ $(uname) == Linux ]; then
-    export CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++17/-std=c++14/g")
-fi
+# if [ $(uname) == Linux ]; then
+#     export CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++17/-std=c++14/g")
+# fi
 
 autoconf
 mkdir build
 cd build
-if [ ! -z ${LIBRARY_PREFIX+x} ]; then
-    ../configure --prefix=$LIBRARY_PREFIX
-else
-    ../configure --prefix=$PREFIX
-fi
+# if [ ! -z ${LIBRARY_PREFIX+x} ]; then
+#     ../configure --prefix=$LIBRARY_PREFIX
+# else
+#     ../configure --prefix=$PREFIX
+# fi
 
 [[ "$target_platform" == "win-64" ]] && patch_libtool
 
